c83e2a2dca7e6ace4daccdb1799aab8e61bf441b
21522683 <21522683@gm.uit.edu.vn>
2022-11-27 01:09:08 +0700

    Tinh up

---
+++ src/assets/images/index.js
@@ -0,0 +1,9 @@
+export {default as ICON_BACK} from './48px.png';
+export {default as AVATAR} from './Ellipse.png';
+export {default as TREE} from './tree.png';
+export {default as PROFILE} from './profile.png';
+export {default as COURSE} from './courses.png';
+export {default as DONATION} from './Donation.png';
+export {default as GIFT} from './Gift.png';
+export {default as VOUCHER} from './voucher.png';
+export {default as ICON} from './Icon.png';
