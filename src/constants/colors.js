84d528ab7653aa54b60b224791a296cf8958101d
Hồ Đình Mạnh <21522327@gm.uit.edu.vn>
2022-11-26 11:01:51 +0700

    initial commit

---
+++ src/constants/colors.js
@@ -0,0 +1,15 @@
+const CUSTOM_COLOR = {
+  vermilion: '#FF460A',
+  white: '#FFFFFF',
+  black: '#000',
+  sunsetOrange: '#FF4B3A',
+  concrete: '#F2F2F2',
+  athensGray: '#F4F4F8',
+  gallery: '#EFEEEE',
+  silverChalice: '#9F9F9F',
+  pink: '#FFC0CB',
+  frenchRose: '#EB4796',
+  blueRibbon: '#0038FF',
+};
+
+export default CUSTOM_COLOR;
