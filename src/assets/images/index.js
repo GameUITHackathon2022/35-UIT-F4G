85319fdb266a07b02b03c5bd3530691aa5bcd032
Hồ Đình Mạnh <21522327@gm.uit.edu.vn>
2022-11-27 01:32:22 +0700

    Manh update

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 export {default as ICON_BACK} from './48px.png';
 export {default as AVATAR} from './Ellipse.png';
 export {default as TREE} from './tree.png';
@@ -8,6 +8,6 @@ export {default as DONATION} from './Donation.png';
 export {default as GIFT} from './Gift.png';
 export {default as VOUCHER} from './voucher.png';
 export {default as ICON} from './Icon.png';
-=======
+
 export {default as IMG_Post1} from './post1.png';
->>>>>>> f6bd2fc0c243ffe48b27a59cc1847303b71febad
+
